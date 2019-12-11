3c677f52e8509bccccd43e59274aa769d6443ec8
Rune Eggers <milehigh.thedane@gmail.com>
2019-12-11 19:25:30 +0100

    changed test range to store result of os system

--- test_range.py
+++ test_range.py
@@ -5,8 +5,8 @@ from options.test_options import TestOptions
 
 def main(model_name, epoch_from, epoch_to, num_test):
     for i in range(epoch_from, epoch_to):
-        os.system("python3 test.py --dataroot ./datasets/bottles/%s --mode cycle_gan --epoch %i --name %s_cyclegan --num_test %s" 
-        % (model_name, i, model_name, num_test))  
+        result = os.system("python3 test.py --dataroot ./datasets/bottles/%s --mode cycle_gan --epoch %i --name %s_cyclegan --num_test %s" 
+        % (model_name, i, model_name, num_test)) 
         
 if __name__ == "__main__":
     opt = TestOptions().parse()
